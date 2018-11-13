15bf94ac84e94e09f3204640b181244b48cc9928
Minesh Patel <mpatel@ebates.com>
2018-11-13 14:56:12 -0800

    Committing restaurants

---
+++ Restaurants/app/src/main/java/poc/ebates/com/restaurants/BaseApplication.kt
@@ -0,0 +1,30 @@
+package com.ebates.restaurants.poc
+
+import android.app.Application
+import ru.terrakok.cicerone.Cicerone
+import ru.terrakok.cicerone.Router
+
+
+class BaseApplication : Application() {
+
+  companion object {
+    lateinit var INSTANCE: BaseApplication
+  }
+
+  init {
+    INSTANCE = this
+  }
+
+  // Routing layer (VIPER)
+  lateinit var cicerone: Cicerone<Router>
+
+  override fun onCreate() {
+    super.onCreate()
+    INSTANCE = this
+    this.initCicerone()
+  }
+
+  private fun BaseApplication.initCicerone() {
+    this.cicerone = Cicerone.create()
+  }
+}
